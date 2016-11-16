69e96b33994b86ec71f3158e2ad37ab0e0991066
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2016-11-16 21:06:06 +0530

    MD5 Hash, code reorganized
    # What's new
    
    ## Major
    
    * A new class stores constants that will be used throughout phases,
    defines output directory (other programs will also be updated later)
    * Replaced hash calculation entirely: MD5 is now used to generate nice
    looking 16-character file names
    
    ## Not so major
    
    * Absolute paths replaced with relative paths via constants
    * Directories are created if they do not exist
    * i < noOfURLs -> (i < noOfURLs && i < urls.size())
    * unnecessary ArrayList historyContent removed - why double the load?
    * now displays which URL is processing nicely (possible use to make
    filenames more bearable)
    * URL Texts is now a constant and changed to urlTexts to avoid conflicts
    because of spaces in the future
    * Escaped backslashes "\\" are replaced by frontslashes for UNIX
    compatibility and in general cleaner look (I think the backslash is
    native to Windows for pathnames)
    * Shifted code from main into a function
    * Documentation comments added
    * Directory cleaning commented out for now?
    
    questions ->
    why store the entire content into an arraylist and then use?

---
+++ src/deimos/common/DeimosConfig.java
@@ -0,0 +1,7 @@
+package deimos.common;
+
+public class DeimosConfig {
+	
+	final public static String OUTPUT_DIR = "output";
+
+}
--- src/deimos/phase1/TextFromURL.java
+++ src/deimos/phase1/TextFromURL.java
@@ -1,19 +1,27 @@
 package deimos.phase1;
 
+import deimos.common.DeimosConfig;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class TextFromURL {
+	
+	final private static String URLS_DIR = DeimosConfig.OUTPUT_DIR + "/urltexts";
+	private static MessageDigest md;
 
-	// this function deletes all the files exist in the URL texts directory
-
-	public static void cleanDirectory() {
-		File file = new File("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts");
+	/**
+	 * Deletes all the files exist in the URL texts directory.
+	 */
+	public static void cleanDirectory() throws SecurityException
+	{
+		File file = new File(URLS_DIR);
 		String[] myFiles;
 		if (file.isDirectory()) {
 			myFiles = file.list();		
@@ -23,82 +31,115 @@ public class TextFromURL {
 			}
 		}
 	}
+	
+	/** 
+	 * Generates an MD5 Hash to use as a file name.
+	 * This helps to determine whether one URL is visited more than once
+	 * @param name The URL to hash
+	 * @return A legible filename with a hash.
+	 */
+	public static String getHashedFileName(String name) {
+		
+		String filename = "";
+		byte[] digest = md.digest(name.getBytes());
+		filename = (new HexBinaryAdapter()).marshal(digest);
 
-	public static void main(String[] args) {
+		return filename;
+	}
+	
+
+	public static void fetchTextFromHistoryDump() {
 		try {
+			
 			int noOfURLs; // represents number of URLs taken into consideration
 			File historyFile = new File("export-history.txt"); 
 		
 			FileReader fileReader = new FileReader(historyFile);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			List<String> historyContent = new ArrayList<String>();
 			List<String> urls = new ArrayList<String>();
 			String line;
 			
 			while ((line = bufferedReader.readLine()) != null) {
-				historyContent.add(line);
+				urls.add(line);
 			}
 			fileReader.close();
+			
+			// remove the first line that has the number of URLs
+			urls.remove(0);
 
 			/* till now, reading of file is done into an arraylist
 			 * following code gets only urls from the above arraylist
 			 */
-
-			for (int i = 1; i < historyContent.size(); i++) {
-				String s = historyContent.get(i);
+			
+			// Initialize the MessageDigest object that lets us produce hashes
+			try {
+				md = MessageDigest.getInstance("MD5");
+				
+			} catch(NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			
+			
+			// Parse the history dump to get URLs only
+			for (int i = 0; i < urls.size(); i++) {
+				String s = urls.get(i);
 				// System.out.println(s);
-				int index = s.indexOf('|');
-				String subst = s.substring(index + 2);
-				urls.add(subst);
+				String subst = s.substring(s.indexOf('|') + 2);
+				urls.set(i, subst);
 			}
 
-			PrintStream fileStream;
+			noOfURLs = 1000; // any value ranging from 1 to urls.size()
+
+			new File(URLS_DIR).mkdirs(); // Creates empty output directory if it doesn't exist
+
+			// cleanDirectory(); // delete previous text files
+
+			System.out.println("Extracting text from fetched URLs...\n");
 
 			try {
-				
-				noOfURLs = 1000; // we can provide any value to it ranging from 1 to urls.size()
-				
-				fileStream = new PrintStream(new File("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts\\URLs.txt")); // contains all urls
-				
-				fileStream.println("Total URLs: "+noOfURLs);
 
-				for (int i = 0; i < noOfURLs; i++) {
-					if(i<10)
-						fileStream.println((i+1)+"	"+urls.get(i));
-					else
-						fileStream.println((i+1)+"	"+urls.get(i));
-				}
+				for (int i = 0; (i < noOfURLs && i < urls.size()) ; i++)
+				{
+					String currentURL = urls.get(i).replace("https://","").
+							replace("http://","");
 
-				cleanDirectory(); // calling the function to delete previous text files
+					String truncURL = currentURL.substring(0, 
+							Math.min(currentURL.length(), 32));
 
-				System.out.println("This program extracts text from fetched URLs");
+					String hashValue = getHashedFileName(currentURL);
 
-				// here I am fetching only first 10 URLs but we need to fetch urls.size()!!!
-				
-				for (int i = 0; i < noOfURLs; i++) {
-					
-					/* here hash value is calculated which represents a file name 
-					 * Now we create a file and check if that file already exists
-					 * this helps to determine whether one URL is visited more than once
-					 */
-					String hashValue = Integer.toString(urls.get(i).hashCode());
-					
-					File f = new File("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts\\"+hashValue);
-					
-					if(!(f.exists()) && !f.isDirectory()) { 
-						PageFetcher.fetchHTMLAsFile("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts\\" + (hashValue)+ ".txt", urls.get(i));
+					System.out.println(i + ": " + truncURL + " " + hashValue);
+
+					File f = new File(URLS_DIR+"/"+hashValue);
+
+					if(!f.isDirectory())
+					{
+						// PROBLEM?!!?
+						if (f.exists() | f.getAbsoluteFile().exists()) {
+							System.out.println("Page already fetched.");
+						}
+						else {
+							PageFetcher.fetchHTMLAsFile(URLS_DIR + "/" +
+								(hashValue)+ ".txt", urls.get(i));
+						}
 					}
 				}
-				
-				System.out.println("All files are fetched!!!");
 
-			} catch (IOException ioe) {
-				ioe.printStackTrace();
+				System.out.println("All files are fetched!");
+
+
+			} catch (SecurityException se) {
+				se.printStackTrace();
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	public static void main(String[] args) {
+
+		fetchTextFromHistoryDump();
 	}
 }
