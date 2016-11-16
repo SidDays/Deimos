956f52968b3b702517a0d1f465c5fa7a553dc4c6
Owner <Owner@LAPTOP-017T0FBF>
2016-11-16 13:15:48 +0530

    Modified TextFromURL a little

--- src/deimos/phase1/TextFromURL.java
+++ src/deimos/phase1/TextFromURL.java
@@ -7,9 +7,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class TextFromURL {
-	
+
 	// this function deletes all the files exist in the URL texts directory
 	// here also directories are local machine dependent
 	public static void cleanDirectory() {
@@ -26,7 +28,10 @@ public class TextFromURL {
 
 	public static void main(String[] args) {
 		try {
-			File historyFile = new File("\\C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\export-history.txt"); //location of history text file, machine dependent!
+			int noOfURLs; // this represents number of URLs taken into
+							// consideration
+			File historyFile = new File("export-history.txt"); 
+		
 			FileReader fileReader = new FileReader(historyFile);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			List<String> historyContent = new ArrayList<String>();
@@ -37,10 +42,10 @@ public class TextFromURL {
 				// historyContent.add("\n");
 			}
 			fileReader.close();
-			
-			//till now, reading of file is done into an arraylist
-			//following code gets only urls from the above arraylist
-			
+
+			// till now, reading of file is done into an arraylist
+			// following code gets only urls from the above arraylist
+
 			for (int i = 1; i < historyContent.size(); i++) {
 				String s = historyContent.get(i);
 				// System.out.println(s);
@@ -52,24 +57,43 @@ public class TextFromURL {
 			PrintStream fileStream;
 
 			try {
-				fileStream = new PrintStream(new File("URLs.txt")); //contains all urls
-				fileStream.println(urls.size());
-				
-				//System.out.println(output.get(i));
-				
-				for (int i = 0; i < urls.size(); i++)
-					fileStream.println(urls.get(i));
+				noOfURLs = 100; // we can provide any value to it ranging from 1 to urls.size()
+				fileStream = new PrintStream(new File("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts\\URLs.txt")); // contains all urls
+				fileStream.println("Total URLs: "+noOfURLs);
+
+				// System.out.println(output.get(i));
+				for (int i = 0; i < noOfURLs; i++) {
+					if(i<10)
+						fileStream.println((i+1)+"	"+urls.get(i));
+					else
+						fileStream.println((i+1)+"	"+urls.get(i));
+				}
 				int count = 1;
-				
-				cleanDirectory(); //calling the function to delete previous text files 
-				
+
+				cleanDirectory(); // calling the function to delete previous text files
+
 				System.out.println("This program extracts text from fetched URLs");
-				//here I am fetching only first 10 URLs but we need to fetch urls.size()!!!
-				for (int i = 0; i < 10; i++)
-					PageFetcher.fetchHTMLAsFile("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts\\URL-text " + (count++) + ".txt", urls.get(i));
+
+				// here I am fetching only first 10 URLs but we need to fetch urls.size()!!!
+				for (int i = 0; i < noOfURLs; i++) {
+					if (i == 99) {
+						PageFetcher.fetchHTMLAsFile("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts\\URL-text "+ (count++) + ".txt", urls.get(i));
+					} else {
+						String currentStr = urls.get(i);
+						String nextStr = urls.get(i + 1);
+						URI currentURI = new URI(currentStr);
+						URI nextURI = new URI(nextStr);
+						if (!(currentURI.equals(nextURI)))
+							PageFetcher.fetchHTMLAsFile("C:\\Users\\Owner\\git\\Deimos-BE-A-2017-KJSCE\\URL Texts\\URL-text " + (count++)+ ".txt", urls.get(i));
+					}
+				}
 				System.out.println("All files are fetched!!!");
+
 			} catch (IOException ioe) {
 				ioe.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 
 		} catch (IOException e) {
