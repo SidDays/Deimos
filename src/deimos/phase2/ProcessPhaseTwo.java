15b4dad06fa44b356eb5c0498672185b51c293bf
Owner <Owner@LAPTOP-017T0FBF>
2016-12-06 16:36:20 +0530

    Updated ProcessPhaseTwo.java a little bit

--- src/deimos/phase2/ProcessPhaseTwo.java
+++ src/deimos/phase2/ProcessPhaseTwo.java
@@ -1,9 +1,19 @@
 package deimos.phase2;
 
+import java.io.File;
+
 public class ProcessPhaseTwo {
-	public static void main() {
+	public static void main(String[] args) {	
 		
 		TextFromURL.fetchTextFromHistoryDump();
 		RemoveStopWords.removeStopWordsFromURLTexts();
+		File directory = new File(deimos.phase2.RemoveStopWords.SWFREE_DIR);
+		File[] cleanedFiles = directory.listFiles();
+		String[] files = new String[cleanedFiles.length];
+		for (int i = 0; i < cleanedFiles.length; i++) {
+			files[i] = cleanedFiles[i].getName();
+			//System.out.println(files[i]);
+		}
+		Stemmer.main(files);
 	}
 }
