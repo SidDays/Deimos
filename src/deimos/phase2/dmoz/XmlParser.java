ca54da7f001614d2927b082e8545adc09d89223c
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2017-02-08 07:35:10 +0530

    XmlParser only prints again (revert Jan-10)
    Removed code of removing nodes from XML (this was done manually)

--- src/deimos/phase2/dmoz/XmlParser.java
+++ src/deimos/phase2/dmoz/XmlParser.java
@@ -1,23 +1,14 @@
 package deimos.phase2.dmoz;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -26,6 +17,7 @@ import org.xml.sax.SAXException;
  * Parses XML data.
  * 
  * Reference:
+ * (unused now)
  * examples.javacodegeeks.com/core-java/xml/dom/remove-nodes-from-dom-document-recursively
  * stackoverflow.com/questions/11883294/writing-to-txt-file-from-stringwriter
  * 
@@ -33,73 +25,92 @@ import org.xml.sax.SAXException;
  * @author Siddhesh Karekar
  *
  */
-public class XmlParser {
 
+public class XmlParser {
+	
 	/**
 	 * Location of XML file to parse.
 	 */
 	final public static String FILE_XML_EXAMPLE = "resources/xmlexample.xml";
-	// final public static String FILE_XML_EXAMPLE = "E:/Downloads/Padhai/Deimos/Dmoz/content.rdf.u8/content.rdf.u8";
-	final public static String FILE_XML_EXAMPLE2 = "export-dmoz.xml";
-
-	public static void removeRecursively(Node node, short nodeType, String name) {
-		if (node.getNodeType()==nodeType && (name == null || node.getNodeName().equals(name))) {
-			node.getParentNode().removeChild(node);
-		}
-		else {
-			// check the children recursively
-			NodeList list = node.getChildNodes();
-			for (int i = 0; i < list.getLength(); i++) {
-				removeRecursively(list.item(i), nodeType, name);
-			}
-		}
-	}
-
-	public static final void prettyPrint(Document xml)
-			throws TransformerConfigurationException, TransformerException, IOException
-	{
-		Transformer tf = TransformerFactory.newInstance().newTransformer();
-		tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-		tf.setOutputProperty(OutputKeys.INDENT, "yes");
-		// Writer out = new StringWriter();
-		FileWriter fw = new FileWriter(FILE_XML_EXAMPLE2);
-		tf.transform(new DOMSource(xml), new StreamResult(fw));
-		System.out.println("Exported cleaned XML to "+FILE_XML_EXAMPLE2+".");
-	}
+	// final public static String FILE_XML_EXAMPLE = "E:/Downloads/Padhai/Deimos/Dmoz/content.rdf.u8/content-noExternalPage.rdf.u8";
+	final public static String FILE_XML_OUTPUT = "export-dmoz.xml";
 	
-	public static void removeNodes()
-			throws TransformerConfigurationException, TransformerException,
-			ParserConfigurationException, FileNotFoundException, SAXException,
-			IOException
-	{
-		
+	public void getAllExternalPages(String fileName) {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		dbf.setValidating(false);
-		DocumentBuilder db = dbf.newDocumentBuilder();
+		DocumentBuilder db = null;
 		
-		Document doc = db.parse(new FileInputStream(new File(FILE_XML_EXAMPLE)));
-		
-		// remove all elements named 'item'
-		removeRecursively(doc, Node.ELEMENT_NODE, "ExternalPage");
-
-		// remove all comment nodes
-		// removeRecursively(doc, Node.COMMENT_NODE, null);
-		
-		// Normalize the DOM tree, puts all text nodes in the
-		// full depth of the sub-tree underneath this node
-		doc.normalize();
-		
-		prettyPrint(doc);
-	}
-
-	public static void main(String[] args) {
-
 		try {
-			removeNodes();
-			
-		} catch (Exception e) {
+			db = dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		File file = new File(fileName);
+		if (file.exists()) {
+			Document doc = null;
+			try {
+				doc = db.parse(file);
+			} catch (SAXException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Element docEle = doc.getDocumentElement();
+
+			// Print root element of the document
+			System.out.println("Root element of the document: "
+					+ docEle.getNodeName());
+			NodeList pageList = docEle.getElementsByTagName("ExternalPage");
+
+			// Print total student elements in document
+			System.out.println("Total: " + pageList.getLength());
+			
+			/*
+			for(int i = 0; i < pageList.getLength(); i++) {
+				System.out.println(pageList.item(i));
+			}
+			*/
+			
+			if(pageList != null && pageList.getLength() > 0)
+			{
+				for(int i = 0; i < pageList.getLength(); i++)
+				{
+					Node node = pageList.item(i);
+					if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+						System.out.println();
+
+						Element e = (Element) node;
+						NodeList nodeList = e.getElementsByTagName("d:Title");
+						System.out.println("ExternalPage:\t"
+								+ nodeList.item(0).getChildNodes().item(0)
+										.getNodeValue());
+						
+						
+						nodeList = e.getElementsByTagName("d:Description");
+						String d = nodeList.item(0).getChildNodes().item(0)
+								.getNodeValue();
+						d.replace("\t", ""); // Not working?
+						System.out.println("Description:\t"
+								+ d);
+
+						nodeList = e.getElementsByTagName("topic");
+						System.out.println("Topic:\t\t"
+								+ nodeList.item(0).getChildNodes().item(0)
+										.getNodeValue());
+					}
+				}
+			}
+			
+			else {
+				System.err.println("Error while parsing!!!");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		XmlParser parser = new XmlParser();
+		parser.getAllExternalPages(FILE_XML_EXAMPLE);
 	}
 }
