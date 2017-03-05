129027c1834f29603aa1e6bee286c24bfc6b7ea5
Bhusp <bhushanpathak2895@gmail.com>
2017-03-05 11:25:15 +0530

    User database operations
    users and tf_users are two tables on which data insertion operations is
    performed

---
+++ src/deimos/phase2/user/UserOperations.java
@@ -0,0 +1,174 @@
+package deimos.phase2.user;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import deimos.common.DeimosConfig;
+import deimos.phase2.DBOperations;
+import deimos.phase2.PageFetcher;
+import deimos.phase2.StemmerApplier;
+import deimos.phase2.StopWordsRemoval;
+
+public class UserOperations {
+	
+	static String urlTimestampSubst;
+	
+	static String urlSubst;
+	
+	static String currentURL;
+	
+	static String currentTimestamp;
+	
+	static String currentURLText;
+	
+	static String query;
+	
+	static List<String> urls = new ArrayList<String>();
+	
+	static List<String> urlTimestamp = new ArrayList<String>();
+	
+	static Map<String, Integer> currentTopicTermCounts;
+	
+	static int noOfURLs = DeimosConfig.LIMIT_URLS_DOWNLOADED;
+	static int user_id = 1;
+	
+	static DBOperations dbo;
+	
+	public static void main(String[] args) {
+		fetchTextFromURL();
+	}
+	
+	public static void fetchTextFromURL() {
+		try {
+			dbo = new DBOperations();
+	        dbo.truncateAllTables();
+			File historyFile = new File("export-history.txt"); 
+			
+			FileReader fileReader = new FileReader(historyFile);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			String line;
+			
+			while ((line = bufferedReader.readLine()) != null) {
+				urls.add(line);
+			}
+			fileReader.close();
+			
+			// remove the first line that has the number of URLs
+			urls.remove(0);
+			/*for(int i = 0; i < urls.size(); i++) {
+				System.out.println(urls.get(i));
+			}*/
+			
+			System.out.println("---------------------------------------------");
+			for (int i = 0; i < noOfURLs; i++) {
+				
+				urlTimestampSubst = urls.get(i).substring(0, urls.get(i).indexOf('|'));
+				System.out.println(urlTimestampSubst);
+				urlTimestamp.add(urlTimestampSubst);
+				
+				urlSubst = urls.get(i).substring(urls.get(i).indexOf('|') + 1);
+				System.out.println(urlSubst);
+				System.out.println("---------------------------------------------");
+				urls.set(i, urlSubst);
+			}
+			
+			for (int i = 0; (i < noOfURLs && i < urls.size()) ; i++)
+			{
+				currentTimestamp = urlTimestamp.get(i);
+				
+				currentURL = urls.get(i);
+
+				/*currentURL = currentURL.substring(0, 
+						Math.min(currentURL.length(), 32));*/
+				
+				System.out.println(currentTimestamp+" | "+currentURL);
+				currentURLText = PageFetcher.fetchHTML(currentURL);
+				
+				currentURLText = StopWordsRemoval.removeStopWordsFromString(currentURLText);
+				
+				/*System.out.println();
+				System.out.println("URL Text:");
+				System.out.println(currentURLText);
+				System.out.println();
+				
+				query = "INSERT INTO users (user_td, url_timestamp, url) VALUES ('"+ 
+						user_id + "','" + "TO_TIMESTAMP('"+currentTimestamp + "','" + "'DD-MON-YY HH.MI.SS')"+"','"+ currentURLText +"')";
+				
+				query = String.format(
+						"INSERT INTO users (user_id, url_timestamp, url) VALUES (%d, '%s', '%s')",
+						user_id,
+						currentTimestamp,
+						currentURL
+						);
+				
+				dbo.executeUpdate(query);*/
+			}
+			
+			/*System.out.println("---------------------------------------------");
+			System.out.println("Text for URL: "+currentURLText);*/
+			
+			Map<String, Integer> porter = StemmerApplier.stemmedWordsAndCount(currentURLText);
+			for (Map.Entry<String, Integer> entry : porter.entrySet())
+			{
+				String stemmedWord = entry.getKey();
+				Integer porterCount = entry.getValue();
+
+				// Update currentTopicTermCounts
+				// If it is not in the HashMap, null will be returned.
+				Integer existingCount = currentTopicTermCounts.get(stemmedWord);
+				if(existingCount == null)
+					currentTopicTermCounts.put(stemmedWord, 1);
+				else
+					currentTopicTermCounts.put(stemmedWord, existingCount + porterCount);
+			}
+			
+			for (Map.Entry<String, Integer> entry : currentTopicTermCounts.entrySet())
+			{
+				String term = entry.getKey();
+				
+				if(term.length() > 50)
+					term = term.substring(0, 50);
+				
+				Integer tf = entry.getValue();
+				System.out.println("*************************************************************");
+				query = String.format(
+						"INSERT INTO tf_user (user_id, term, tf, weight) VALUES (%d, '%s', %d, null)",
+						user_id,
+						term,
+						tf
+						);
+				
+				dbo.executeUpdate(query);
+				System.out.println("---------------------------------------------");
+				//System.out.println(query);
+				
+			}
+			
+			
+		}
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (SQLException e) {
+			
+			// e.printStackTrace();
+			System.out.println(e);
+		}
+		catch (Exception ex) {
+			System.err.println("Skipping this URL.");
+		}
+		
+	}
+}
